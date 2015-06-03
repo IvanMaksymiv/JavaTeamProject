236d234458ddde6d4ff839cba2be69be67f7ed85
IvanMaksymiv <ivan-qa@drucode.com>
2015-06-03 11:29:49 +0300

    added project to git

---
+++ src/com/team3/www/EmployeeDTO.java
@@ -0,0 +1,57 @@
+package com.team3.www;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EmployeeDTO {
+    private String lastName;
+    private String position;
+    private int salary;
+
+    @JsonCreator
+    public EmployeeDTO(@JsonProperty("lastName") String lastName,
+                       @JsonProperty("position") String position,
+                       @JsonProperty("salary") int salary){
+        this.lastName = lastName;
+        this.position = position;
+        this.salary = salary;
+
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+@Override
+public String toString() {
+        return "[lastName=" + this.lastName + ", position=" + this.position + ", salary="
+        + this.salary + "]";
+        }
+}
+
+
+
+
---
+++ src/com/team3/www/JacksonUse.java
@@ -0,0 +1,57 @@
+package com.team3.www;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class JacksonUse {
+    public static void main(String[] args) {
+        InputStream input = null;
+        try {
+            input = new FileInputStream("src/com/team3/www/employee.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+        TypeFactory typeFactory = TypeFactory.defaultInstance();
+
+        List<EmployeeDTO> employee = null;
+        try {
+            employee = objectMapper.readValue(input,
+                    typeFactory.constructCollectionType(
+                            ArrayList.class, EmployeeDTO.class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Arrays.sort(employee.toArray());
+        // System.out.print(employee);
+
+
+        System.out.println(employee.get(0).getLastName() + " " +
+                employee.get(0).getPosition() + " " +
+                employee.get(0).getSalary());
+        System.out.println(employee.get(1).getLastName() + " " +
+                employee.get(1).getPosition() + " " +
+                employee.get(1).getSalary());
+        System.out.println(employee.get(2).getLastName() + " " +
+                employee.get(2).getPosition() + " " +
+                employee.get(2).getSalary());
+        System.out.println(employee.get(3).getLastName() + " " +
+                employee.get(3).getPosition() + " " +
+                employee.get(3).getSalary());
+        System.out.println(employee.get(4).getLastName() + " " +
+                employee.get(4).getPosition() + " " +
+                employee.get(4).getSalary());
+        System.out.println("------------------------");
+    }
+}
